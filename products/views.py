16f32ea22d9f7d8e532e3caf5702880d6bbbbeb0
lisa1Q84 <lisafeigen@web.de>
2021-07-18 13:49:44 +0000

    add sorting and product count to product page

--- products/views.py
+++ products/views.py
@@ -1,17 +1,56 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+
 # Create your views here.
 
 
+# ALL PRODUCTS
 def all_products(request):
     """ a view to return the product page"""
     products = Product.objects.all()
+    query = None
+    sort = None
+    direction = None
+
+    if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products.annotate(lower_name=Lower('name'))
+            if sortkey == 'categroy':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'dsc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You did not enter a search query")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
+        'search_term': query,
+        'current_sorting': current_sorting,
     }
     return render(request, 'products/products.html', context)
 
 
+# PRODUCT DETAILS
 def product_detail(request, product_id):
     """ a view to return the product detail page"""
     product = get_object_or_404(Product, pk=product_id)
