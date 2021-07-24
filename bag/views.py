407da3100afb9777c9c00ec3ec2ea9ea2f82878c
lisa1Q84 <lisafeigen@web.de>
2021-07-24 14:30:51 +0000

    added product variation models and started building cart view to include variations

--- bag/views.py
+++ bag/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from products.models import Product, Variation
+from django.contrib import messages
+
 
 
 def view_bag(request):
     """ a view to return the shopping bag page"""
     return render(request, 'bag/bag.html')
 
+    """ a view to add items to the shopping bag"""
 
 def add_to_bag(request, item_id):
-
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+    for item in request.POST:
+        key = item
+        val = request.POST[key]
+    if 'type' in request.POST:
+        type = request.POST['type']
+        print(type)
+    if 'duration' in request.POST:
+        duration = request.POST['duration']
+        print(duration)
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
@@ -18,5 +32,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
-    return redirect(redirect_url)
+    return HttpResponseRedirect(reverse("view_bag"))
--- products/models.py
+++ products/models.py
@@ -30,3 +30,34 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class VariationManager(models.Manager):
+    def all(self):
+        return super(VariationManager, self).filter(active=True)
+
+    def types(self):
+        return self.all().filter(category='type')
+
+    def durations(self):
+        return self.all().filter(category='duration')
+
+
+VAR_CATEGORIES = (
+    ('type', 'type'),
+    ('duration', 'duration')
+)
+
+
+class Variation(models.Model):
+    category = models.CharField(max_length=120, choices=VAR_CATEGORIES, default='type')
+    image = models.ImageField(null=True, blank=True)
+    name = models.CharField(max_length=120)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    active = models.BooleanField(default=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.name
