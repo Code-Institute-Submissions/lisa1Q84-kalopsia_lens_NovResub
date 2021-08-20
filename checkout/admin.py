1e33362142c42c0c46b17c253e29dfa13172ca77
lisa1Q84 <lisafeigen@web.de>
2021-08-20 15:44:30 +0000

    established payment flow to stripe and entry to db with only grand total problem to fix

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,7 +11,7 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'date',
-                        'order_total','grand_total',)
+                       'order_total','grand_total',)
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
