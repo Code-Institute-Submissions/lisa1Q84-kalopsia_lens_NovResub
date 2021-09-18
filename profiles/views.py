33e68cc3b9d928d0eb539613fb55e22ad6d7a387
lisa1Q84 <lisafeigen@web.de>
2021-09-18 11:41:11 +0000

    updated profile and fixed order history feature to display variation.added isaved info to profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+
+from checkout.models import Order
 
 
 def profile(request):
     """ Display the user's profile. """
 
     profile = get_object_or_404(UserProfile, user=request.user)
-     if request.method == 'POST':
+    if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
@@ -22,6 +25,23 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is to confirm your past order number {order_number}. '
+        'You have received an email on the date you ordered.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
