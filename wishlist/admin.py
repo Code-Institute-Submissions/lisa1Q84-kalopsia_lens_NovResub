d813329ae78da296cc0311d062a5babfd1db9493
lisa1Q84 <lisafeigen@web.de>
2021-09-28 11:36:22 +0000

    added and tested url for wishlist view

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -2,6 +2,5 @@
 from django.contrib import admin
 from wishlist.models import Wishlist, WishlistItem
 
-
 admin.site.register(Wishlist)
 admin.site.register(WishlistItem)
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from profiles.models import UserProfile
+from products.models import Product
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    """ A model to make the connection between a bookmarked product and a user """
+    user = models.OneToOneField(UserProfile, null=False, blank=False, on_delete=models.CASCADE, related_name='wishlist')
+
+    
+
+    def __str__(self):
+        return f'Wishlist ({self.user})'
+
+
+class WishlistItem(models.Model):
+    """A many to many model to put products on the wishlist"""
+    wishlist = models.ForeignKey(Wishlist, null=False, blank=False, on_delete=models.CASCADE, related_name='wishlist_items')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE, related_name='wishlist_products')
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
