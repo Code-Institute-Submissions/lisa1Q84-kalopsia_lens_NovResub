d84d6ffbc126293b66b18ab486aa59606a4f838f
lisa1Q84 <lisafeigen@web.de>
2021-09-28 09:54:45 +0000

    create wishlist app and create view to show wishlist.copy bag template to turn into wishlist template next

--- kalopsia_lens/settings.py
+++ kalopsia_lens/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'wishlist',
+
     'crispy_forms',
 ]
 
---
+++ wishlist/admin.py
@@ -0,0 +1,7 @@
+# Register your models here.
+from django.contrib import admin
+from wishlist.models import Wishlist, WishlistItem
+
+
+admin.site.register(Wishlist)
+admin.site.register(WishlistItem)
