d640230b12ec0481522e322fc2deec08005f2d76
lisa1Q84 <lisafeigen@web.de>
2021-10-05 22:16:47 +0000

    fixed layout of the shopping bag and wishlist page as well as the index page

--- bag/context.py
+++ bag/context.py
@@ -7,15 +7,15 @@ from decimal import Decimal
 def bag_contents(request):
 
     bag_items = []
-    total = 0
+    bag_total = 0
     product_count = 0
     bag = request.session.get('bag', {})
-    print(bag)
+
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
+            bag_total += item_data * product.price
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
@@ -26,7 +26,7 @@ def bag_contents(request):
             product = get_object_or_404(Product, pk=item_id)
             for variation, quantity in item_data['variation_dict'].items():
                 variation_object = Variation.objects.get(name__icontains=variation)
-                total += quantity * product.price
+                bag_total += quantity * product.price
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
@@ -35,23 +35,23 @@ def bag_contents(request):
                     'variation': variation,
                 })
 
-        if total < settings.FREE_DELIVERY_THRESHOLD:
-            delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-            free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    else:
-        delivery = 0
-        free_delivery_delta = 0
+        if bag_total < settings.FREE_DELIVERY_MIN:
+            delivery_charge = Decimal(settings.DELIVERY_CHARGE)
+            free_delivery_delta = settings.FREE_DELIVERY_MIN - bag_total
+        else:
+            delivery_charge = 0
+            free_delivery_delta = 0
 
-    grand_total = delivery + total
+        bag_grand_total = delivery_charge + bag_total
 
-    context = {
-        'bag_items': bag_items,
-        'total': total,
-        'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total': grand_total,
-    }
+        context = {
+            'bag_items': bag_items,
+            'product_count': product_count,
+            'bag_total': bag_total,
+            'free_delivery_delta': free_delivery_delta,
+            'free_delivery_min': settings.FREE_DELIVERY_MIN,
+            'bag_grand_total': bag_grand_total,
+            'delivery_charge': delivery_charge,
+            }
 
     return context
