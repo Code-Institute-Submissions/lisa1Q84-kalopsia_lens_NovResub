6a1301a1b6f20cb49124efcd9827139c8eeeca5f
lisa1Q84 <lisafeigen@web.de>
2021-10-13 18:24:13 +0000

    corrected AWS Storage bucket name variable

--- kalopsia_lens/settings.py
+++ kalopsia_lens/settings.py
@@ -195,7 +195,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{lisa1q84-kalopsyabooks}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
