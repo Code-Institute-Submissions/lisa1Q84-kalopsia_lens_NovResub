19afdbc9cc14efb9c455c2183d9783078bca6cbd
lisa1Q84 <lisafeigen@web.de>
2021-10-14 21:00:16 +0000

    accidentally committed env.py file so I changed all keys and deleted the file from github

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("SECRET_KEY", "bMMa4drCnMW6>>PO|78i;p@(IFP5H;")
-os.environ.setdefault("DATABASE_URL", "postgres://mcghtecsjoejyn:b9038309141c986c949ae01593cb2cf5691fff509e6a773b98032f874ef0bd2b@ec2-63-32-7-190.eu-west-1.compute.amazonaws.com:5432/d66ct0osrc28gt")
-os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAQR722S34JPGQNXWM")
-os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
+os.environ.setdefault("SECRET_KEY", "r3nv*1aby-1x93pgt*oxu85^2(p)8**k$54bbhy%2%4!mqs6(6")
+os.environ.setdefault("DATABASE_URL", "postgres://spcwpahvcbewbv:df44827357a5f990317781faf50fceafcb76b1b0f06ff247e5d9ae0ded494afc@ec2-54-74-14-109.eu-west-1.compute.amazonaws.com:5432/ddhu35kovi4ce5")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAQR722S34E7MQK7ZH")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
