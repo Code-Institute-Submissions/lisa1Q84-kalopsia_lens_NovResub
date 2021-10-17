9589381583c489074033a81ed6888f98f48d5eda
lisa1Q84 <lisafeigen@web.de>
2021-10-17 16:21:39 +0000

    remove development variable from heroku

--- kalopsia_lens/settings.py
+++ kalopsia_lens/settings.py
@@ -226,9 +226,9 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 
-if 'WRONG' in os.environ:
+if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'kalopsia@example.com'
+    DEFAULT_FROM_EMAIL = 'kalopsyabooks@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
