6e19a206aabd0a6855d983fe6a88170dba89adac
lisa1Q84 <lisafeigen@web.de>
2021-10-19 11:15:56 +0000

    fixed footer to bottom

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,24 @@ from django.db import models
 
 
 class BlogPost(models.Model):
-    title = models.CharField(max_length=52, null=False, blank=False)
-    article = models.TextField(max_length=450, null=False, blank=False)
-    date = models.DateTimeField(auto_now_add=True)
+    """
+    Model for a basic blog post with title, article and date
+    """
+    title = models.CharField(
+        max_length=52,
+        null=False,
+        blank=False
+    )
+
+    article = models.TextField(
+        max_length=450,
+        null=False,
+        blank=False
+    )
+
+    date = models.DateTimeField(
+        auto_now_add=True
+    )
 
     def __str__(self):
         return self.title
