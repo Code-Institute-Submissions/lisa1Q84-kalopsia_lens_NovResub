ee6ab0b16a8a003bd1a03727335f0f894a5832bf
lisa1Q84 <lisafeigen@web.de>
2021-10-19 17:23:11 +0000

    removed variations from the bag app

--- bag/context.py
+++ bag/context.py
@@ -1,7 +1,7 @@
+from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
-from decimal import Decimal
 
 
 def bag_contents(request):
@@ -10,7 +10,6 @@ def bag_contents(request):
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
-    print(bag)
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
@@ -22,35 +21,26 @@ def bag_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
-        else:
-            product = get_object_or_404(Product, pk=item_id)
-            for variation, quantity in item_data['variation_dict'].items():
-                variation_object = Variation.objects.get(name__icontains=variation)
-                total += quantity * product.price
-                product_count += quantity
-                bag_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'product': product,
-                    'variation': variation,
-                })
 
-        if total < settings.FREE_DELIVERY_THRESHOLD:
-            delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-            free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery_charge = total * Decimal(
+                          settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+
     else:
-        delivery = 0
+        delivery_charge = 0
         free_delivery_delta = 0
 
-    grand_total = delivery + total
+    bag_grand_total = delivery_charge + total
 
     context = {
         'bag_items': bag_items,
-        'total': total,
         'product_count': product_count,
-        'delivery': delivery,
+        'total': total,
         'free_delivery_delta': free_delivery_delta,
-        'grand_total': grand_total,
+        'free_delivery_min': settings.FREE_DELIVERY_THRESHOLD,
+        'bag_grand_total': bag_grand_total,
+        'delivery_charge': delivery_charge,
     }
 
     return context
--- bag/views.py
+++ bag/views.py
@@ -5,57 +5,31 @@ from django.contrib import messages
 from products.models import Product
 
 
-"""a view to return the shopping bag page"""
-
-
 def view_bag(request):
+    """a view to show the shopping bag page"""
     return render(request, 'bag/bag.html')
 
 
-"""a view to add items to the shopping bag"""
-
-
 def add_to_bag(request, item_id):
+    """a view to add items to the shopping bag"""
+
     product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    variation = None
     redirect_url = request.POST.get('redirect_url')
-
-    if 'product_variation' in request.POST:
-        variation = request.POST['product_variation']
 
     bag = request.session.get('bag', {})
 
-    if variation:
-        if item_id in list(bag.keys()):
-            if variation in bag[item_id]['variation_dict'].keys():
-                bag[item_id]['variation_dict'][variation] += quantity
-                messages.success(
-                    request, f'updated the quantity of {variation}{product.name} to {bag[item_id]["variation_dict"][variation]}!')
-
-            else:
-                bag[item_id]['variation_dict'][variation] = quantity
-                messages.success(request, f'added variation {variation.upper()}{product.name} to your bag!')
-
-        else:
-            bag[item_id] = {'variation_dict': {variation: quantity}}
-            messages.success(request, f'added {variation.upper()}{product.name} to your bag!')
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+        messages.success(
+                        request, f'You updated {product.name} quantity to {bag[item_id]}!')
 
     else:
-
-        if item_id in list(bag.keys()):
-            bag[item_id] += quantity
-            messages.success(request, f'updated {product.name} quantity to {bag[item_id]}!')
-
-        else:
-            bag[item_id] = quantity
-            messages.success(request, f'added {product.name} to your bag!')
+        bag[item_id] = quantity
+        messages.success(request, f'You added {product.name} to your bag!')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-
-
-"""a view to UPDATE items quantity in the shopping bag"""
 
 
 def update_bag(request, item_id):
@@ -63,33 +37,17 @@ def update_bag(request, item_id):
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    variation = None
-
-    if 'variation' in request.POST:
-        variation = request.POST['variation']
     bag = request.session.get('bag', {})
 
-    if variation:
-        if quantity > 0:
-            bag[item_id]['variation_dict'][variation] = quantity
-            messages.success(request, f'updated quantity of {variation} to {bag[item_id]["variation_dict"][variation]}!')
-
-        else:
-            del bag[item_id]['variation_dict'][variation]
-            messages.success(
-                request, f'removed {variation.upper()}{product.name} from your bag!')
+    if quantity > 0:
+        bag[item_id] = quantity
+        messages.success(
+            request, f'You updated quantity of {product.name} to {bag[item_id]}!')
 
     else:
-
-        if quantity > 0:
-            bag[item_id] = quantity
-            messages.success(
-                request, f'updated quantity of {product.name} to {bag[item_id]}!')
-
-        else:
-            bag.pop[item_id]
-            messages.success(
-                request, f'updated quantity of {product.name} to to {bag[item_id]}')
+        bag.pop[item_id]
+        messages.success(
+            request, f'You updated quantity of {product.name} to {bag[item_id]}')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -97,24 +55,16 @@ def update_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """a view to REMOVE an item in the shopping bag"""
+    
+    bag = request.session.get('bag', {})
 
     try:
         product = get_object_or_404(Product, pk=item_id)
-        variation = None
-        if 'variation' in request.POST:
-            variation = request.POST['variation']
-        bag = request.session.get('bag', {})
-
-        if variation:
-            del bag[item_id]['variation_dict']
-            bag.pop(item_id)
-            messages.success(request, f'removed {variation.upper()}{product.name} from your bag!')
-        else:
-            bag.pop(item_id)
-            messages.success(
-                request, f'removed {product.name} from your bag!')
-
+        bag.pop(item_id)
+        messages.success(
+            request, f'You removed {product.name} from your bag!')
         return HttpResponse(status=200)
+
     except Exception as e:
-        messages.error(request, f'error removing item: {e}')
+        messages.error(request, f'There was an error removing item: {e}')
         return HttpResponse(status=500)
